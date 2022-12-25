043ac3739950e343f1363f99cde53ba9cc0e2f00
Pravindu <pravindubh@gmail.com>
2022-12-25 15:43:58 +0530

    First Commit on this partner dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,478 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="SampleUser">
+        <FaUserCircle className="UserIcon"></FaUserCircle>
+        Sample User
+      </div>
+      <div className="PartnerDashbord">
+        <b className="PartnerDashbordtxt">Partner Dashbord</b>
+      </div>
+      <div className="SecondBackground">
+        <div>
+        <div className="FirstRow">
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+        </div>
+        <div className="FirstRow">
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+        </div>
+        <div className="FirstRow">
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+          <div className="FirstOne">
+            <div className="Upper">
+              <p className="Heads">
+                <div className="Head">
+                  <b className="head1">General Manager</b>
+                  <br />
+                  <b className="head2">hSenid Business</b>
+                </div>
+                <div className="Notify">
+                  <a href="#" className="notification">
+                    <span>Notify</span>
+                    <span class="badge">3</span>
+                  </a>
+                </div>
+              </p>
+            </div>
+            <div className="Middle">
+              <div>
+                <img
+                  className="IMG"
+                  src="logo192.png"
+                  alt="Photo"
+                  width="30"
+                  height="30"
+                ></img>
+                <b className="Client">Client Profile Details</b>
+              </div>
+              <br />
+              <div className="Content">
+                Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                Pravindu
+                <br />
+                Designation : General
+                <br />
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                sample@hSenidbiz.com
+                <br />
+                Tel.No. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                +94705561511
+                <br />
+                More details :{" "}
+                <a href="https://www.w3schools.com/">View Client</a>
+              </div>
+            </div>
+            <div className="Bottom">
+              <button className="ViewButton">View Dashbord</button>
+            </div>
+          </div>
+        </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
